28666fe920d3360c7e3ccb7c3d6b114ee4bb9c01
Vyshnav <116252618+VYSHNAV-PR@users.noreply.github.com>
2026-01-05 21:47:02 +0530

    logical-3

---
+++ logical3.py
@@ -0,0 +1,9 @@
+#1.You are given two strings: haystack and needle. Your task is to determine the first position
+# where the substring needle appears in the string haystack. If the substring needle does not exist
+# in haystack, you should return -1.
+
+def logic(heystack,needle):
+    return heystack.find(needle)
+print(logic("hello","ll"))
+    
+
